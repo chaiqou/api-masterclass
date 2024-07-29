0e2ed2302e7c46731fc0ece8d421bc13c5d6ff9c
Nikoloz Lomtadze <84349052+chaiqou@users.noreply.github.com>
2024-07-29 11:48:25 +0400

    feat: apply api routes

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,10 @@
 <?php
-
-use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function () {
+Route::prefix('api/v1')->group(function () {
     require_once __DIR__.'/api_v1.php';
 });
 
-Route::prefix('v2')->group(function () {
+Route::prefix('api/v2')->group(function () {
     require_once __DIR__.'/api_v2.php';
 });
