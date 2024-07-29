b7157ad26abb585aa4f575594754a402132041e0
Nikoloz Lomtadze <84349052+chaiqou@users.noreply.github.com>
2024-07-29 21:42:02 +0400

    feat: defent routes

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -4,8 +4,11 @@ use App\Http\Controllers\API\V1\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-Route::apiResource('tickets', TicketController::class);
+    Route::apiResource('tickets', TicketController::class);
+});
+
